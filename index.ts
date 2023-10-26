1b8e16b386ed812fd12522f164975f10cb335126
Raphael Fleury <raphael.fleury9@gmail.com>
2023-10-26 15:41:22 -0300

    Serving HTML via Elysia

--- index.ts
+++ index.ts
@@ -1,7 +1,23 @@
 import { Elysia } from "elysia"
+import { html } from "@elysiajs/html"
 
 const app = new Elysia()
-    .get("/", () => "Hello World!")
+    .use(html())
+    .get("/", () => baseHtml)
     .listen(3000)
 
 console.log(`Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
+
+const baseHtml = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo List</title>
+</head>
+<body>
+    Hello World!
+</body>
+</html>
+`
