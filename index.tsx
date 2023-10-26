e718cfc39f22e1ef2f80523a7e7ad674db487294
Raphael Fleury <raphael.fleury9@gmail.com>
2023-10-26 17:31:55 -0300

    Switching from Bootstrap to Tailwind and showing todo list

--- index.tsx
+++ index.tsx
@@ -6,14 +6,13 @@ const app = new Elysia()
     .use(html())
     .get("/", () =>
         <BaseHtml>
-            <div class="vw-100 vh-100 d-flex justify-content-center align-items-center">
-                <button class="btn btn-primary" hx-get="/data" hx-swap="outerHTML">
-                    Click Me
-                </button>
-            </div>
+            <body
+                class="flex w-full h-screen justify-center items-center"
+                hx-get="/todos" hx-trigger="load" hx-swap="innerHTML"
+            />
         </BaseHtml>
     )
-    .get("/data", () => <h1>Clicked</h1>)
+    .get("/todos", () => <TodoList todos={db}/>)
     .listen(3000)
 
 console.log(`Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
@@ -26,8 +25,38 @@ const BaseHtml = ({ children }: elements.Children) => `
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo List</title>
     <script src="https://unpkg.com/htmx.org@1.9.3"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>${children}</body>
+${children}
 </html>
 `
+
+type Todo = {
+    id: number
+    content: string
+    completed: boolean
+}
+
+const db: Todo[] = [
+    { id: 1, content: "Do the dishes", completed: true },
+    { id: 2, content: "Study Mathematics", completed: false }
+]
+
+const TodoItem = ({id, content, completed}: Todo) => (
+    <div class="flex flex-row space-x-3">
+        <p>{content}</p>
+        <input type="checkbox" checked={completed} />
+        <button class="text-red-500">X</button>
+    </div>
+)
+
+function TodoList({todos}: {todos: Todo[]}) {
+    if (!todos.length)
+        return <p>No Tasks!</p>
+
+    return <div>
+        {todos.map(todo => (
+            <TodoItem {...todo} />
+        ))}
+    </div>
+}
