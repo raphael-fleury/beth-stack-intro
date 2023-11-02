508ad4438a4b96918ce924c033c7c221c016ccca
Raphael Fleury <raphael.fleury9@gmail.com>
2023-11-02 14:18:51 -0300

    Cleaning form on submit

--- src/components/BaseHtml/index.tsx
+++ src/components/BaseHtml/index.tsx
@@ -9,6 +9,7 @@ export const BaseHtml = ({ children }: elements.Children) => `
         <title>Todo List</title>
         <script src="https://unpkg.com/htmx.org@1.9.3"></script>
         <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
     </head>
     ${children}
 </html>
